deb59b95faec43f3b479e81438fbb1b8ff91f979
Steven Lawson <visual.infusion@gmail.com>
2020-08-08 13:49:13 -0500

    - add user seeding

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = new User;
+        $admin->name = 'Admin';
+        $admin->email = 'visual.infusion@gmail.com';
+        $admin->password = Hash::make('test1234');
+        $admin->save();
+    }
+}
